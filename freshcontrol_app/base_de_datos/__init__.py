9fb32bab299111cd8ea6206b54495ac0ca806974
Rodriguez Martin <martinleonelr38@email.com>
2026-09-21 21:31:39 -0300

    fix: corregir exportaciones de init en paquete base_de_datos

--- freshcontrol_app/base_de_datos/__init__.py
+++ freshcontrol_app/base_de_datos/__init__.py
@@ -1,6 +1,3 @@
-from .gestor_bd import GestorBD
-from .modelos import inicializar_tablas
-
 from .gestor_bd import inicializar_bd, registrar_usuario, verificar_credenciales
 
 __all__ = ["inicializar_bd", "registrar_usuario", "verificar_credenciales"]
